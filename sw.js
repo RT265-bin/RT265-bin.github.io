5e21271924d3298f46e92cc0a0c94d9f9fbe0dc7
RT265-bin <t2976376589@outlook.com>
2025-08-10 16:06:08 +0800

    Site updated: 2025-08-10 16:06:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1754808585461";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1754813157181";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
